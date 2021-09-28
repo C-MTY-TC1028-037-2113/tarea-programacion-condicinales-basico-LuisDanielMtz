bc2bb384e14b55c0a4edc65f2223f98bae71058d
Luis Daniel <A00834087@itesm.mx>
2021-09-28 18:33:05 -0500

    problema 6

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
-    
+    peso = input("Peso en kg: ")
+    Altura =input("Altura en m: ")
+    if peso == 0 or Altura == 0:
+        print ("Revisa tus datos, alguno de ellos es erróneo.")
+    else :
+        imc=peso/ Altura**2
+        if  imc < 20:
+            print ("PESO BAJO")
+        elif 20 <= imc < 25:
+            print("NORMAL")
+        elif 25 <= imc < 30:
+            print("SOBREPESO")
+        elif 30 <= imc < 40:
+            print("SOBREPESO")
+        else:
+            print("OBESIDAD MORBIDA")
 
 if __name__=='__main__':
     main()
